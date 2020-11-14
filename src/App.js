8ac6d63c8d7d5a84e4ea5a15aad1f23d5be3d6ea
Jean-Poul <cph-jl360@cphbusiness.dk>
2020-11-14 11:54:03 +0100

    refactoring of header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./style1.css";
+import "./style2.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
 import AllJokes from "./AllJokes";
@@ -13,7 +13,7 @@ const Header = ({ isLoggedIn, loginMsg, isAdmin, loginName }) => {
   return (
     <>
       <Navbar bg="dark" variant="dark" id="header">
-        <Navbar.Brand href="#home">Hold E, Gruppe 8</Navbar.Brand>
+        <Navbar.Brand href="#home">JPLM Designs</Navbar.Brand>
         <Nav className="mr-auto">
           <NavLink
             className="nav-link"
